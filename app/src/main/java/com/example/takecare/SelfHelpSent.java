8ebb47d27e07581dc6546989cc8d681f37e0b018
EricPillai <eric.pillai01@gmail.com>
2019-02-16 21:55:39 -0800

    yeet

---
+++ app/src/main/java/com/example/takecare/SelfHelpSent.java
@@ -0,0 +1,13 @@
+package com.example.takecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelfHelpSent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_help_sent);
+    }
+}
