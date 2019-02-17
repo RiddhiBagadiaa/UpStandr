4203bf78ff3e2ea1d430920e2db496a234b63c84
H Mandavia <hmandavia@Hs-MacBook-Air.local>
2019-02-16 22:16:22 -0800

    second commit

---
+++ app/src/main/java/com/example/takecare/thank_you_page.java
@@ -0,0 +1,14 @@
+package com.example.takecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thank_you_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you_page);
+    }
+}
